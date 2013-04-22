38c171d9afefbfce8364794fa02fc8114d1b6a42
Stuart <stuart.quin@gmail.com>
2013-04-22 10:08:34 +0000

    Error out if id is not set in request

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ program
 
 var cacheType = program.cache || 'redis';
 var redisPort = program.redis || 6379;
-var appPort = program.port || 3000;
+var appPort = program.port || 4000;
 
 var express = require('express');
 var app = express();
@@ -31,11 +31,14 @@ app.all('/', function(req, res, next) {
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    comments.fetch(req.query.id, function(error,data){
-        res.send(data);
-    });
+    if ( req.query.id ){
+        comments.fetch(req.query.id, function(error,data){
+            res.send(data);
+        });
+    } else {
+        res.send({error: "No ID specified"});
+    }
 });
-
 
 app.listen(appPort);
 console.log('Server started on port '+appPort);
